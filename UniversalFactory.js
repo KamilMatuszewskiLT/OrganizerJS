6afe65cd5bc6c1296a89b54510abd69f7cb5a37b
liado <liado@wp.pl>
2018-04-17 11:15:14 +0200

    commit

--- UniversalFactory.js
+++ UniversalFactory.js
@@ -22,15 +22,21 @@ function addFromXMLDB(target, DBname, tagName) {
                 record.setAttribute("class", "Record");
 
                 for (let j = 0; j < myObj[i].attributes.length; j++) { // Create an element for each XML attribute.
-                    var nameD = document.createElement("div");
-                    nameD.setAttribute("class", "dataType");
-                    nodeName = myObj[i].attributes[j].nodeName;
-                    nameD.innerHTML = nodeName.charAt(0).toUpperCase() + nodeName.slice(1) + ":"; // Capitalize each XML name.
+                    if(myObj[i].attributes[j].nodeName === "map"){
+                        var name = makeMapHref(myObj[i].attributes[j].nodeValue);
+                    } else {
                     var name = document.createElement("div");
                     name.setAttribute("class", "dataField");
                     name.innerHTML = myObj[i].attributes[j].nodeValue;
+                    var nameD = document.createElement("div");
+                    nameD.setAttribute("class", "dataType");
+                    let nodeName = myObj[i].attributes[j].nodeName;
+                    nameD.innerHTML = nodeName.charAt(0).toUpperCase() + nodeName.slice(1) + ":"; // Capitalize each XML name.
+                    
                     record.appendChild(nameD);
+                }
                     record.appendChild(name);
+                    
                 }
 
                 content.appendChild(record);
@@ -94,7 +100,7 @@ function makeInputFilter(target){
 }
 
 function makeToggleableButton(){
-    var toggle = document.createElement("button");
+    var toggle = document.createElement("button"); 
             toggle.setAttribute("class", "toggleButton");
             toggle.innerHTML = "Normal display";
             toggle.addEventListener("click", function () {
@@ -106,4 +112,14 @@ function makeToggleableButton(){
                 }
             });
     return toggle;
+}
+
+function makeMapHref(link){
+    var mapAElement  = document.createElement("a");
+     mapAElement .setAttribute('href','images'+link);
+    var href = document.createElement("img");
+    href.setAttribute('src','images'+link);
+    href.setAttribute('class','mapImage');
+     mapAElement .appendChild(href);
+    return  mapAElement ;
 }
