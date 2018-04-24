53a0497606e077055cc13c03c2b44181f6204759
liado <liado@wp.pl>
2018-04-24 14:26:35 +0200

    Done for the day.

--- ObjectFactory.js
+++ ObjectFactory.js
@@ -5,59 +5,71 @@
  */
 
 function TestFunction() {
-    var test = objectsFromDB().getInstance;
+    var test = objectsFromDB.getInstance();
+    console.log(test.Characters[0]);
+    console.log("This many characters in DB: " + test.Characters.length);
     for (let i = 0; i < test.Characters.length; i++) {
-        console.log(test.Characters.values(i));
+        console.log(test.Characters.values());
     }
 }
 
-var objectsFromDB = function () {
+var objectsFromDB = (function () {
     var instance;
     var characters = CreateObjectsFromDB('CharactersDB.xml', 'character');
-    var locations = CreateObjectsFromDB('LocationsDB.xml', 'locations');
-
-    Characters = function(){return this.characters;};
-    Locations = function(){return this.locations;};
-
-    return {
+    
+    Characters = function () {
+        console.log("In objectsFromDB: " + this);
+        return this.characters;
+        
+    };
+    
+    function createInstance() {
+        var object = new Object("I am the instance");
+        return object;
+    }
+        return {
         getInstance: function () {
-            if (instance === null) {
-                instance = new objectsFromDB();
-                // Hide the constructor so the returned objected can't be new'd...
-                instance.constructor = null;
+            if (!instance) {
+                instance = createInstance();
             }
             return instance;
         }
     };
-};
+})();
 
 function CreateObjectsFromDB(DBname, tagName) {
     var charFactory = new Factory();
     var xmlDocument = new XMLHttpRequest();
-    var newObject = {};
+    var allObjects = [];
     xmlDocument.onreadystatechange = function () {
         //console.log("State " + this.readyState + " Status " + this.status);    
         if (this.readyState === 4 && this.status === 200) {
+            console.log("Opened db.");
             var xmlDoc = xmlDocument.responseXML;
             var myObj = xmlDoc.getElementsByTagName(tagName);
-            newObject = charFactory.createObject(myObj);
+            allObjects = charFactory.createObject(myObj);
+            console.log("In CreateObjectsFromDB() if statements: " + allObjects[0].name);
         }
     };
     xmlDocument.open("GET", DBname, true);
     xmlDocument.send();
-    return newObject;
+    console.log("In CreateObjectsFromDB(): " + allObjects[0].name);
+    return allObjects;
+    
 }
 
 function Factory() {
 
     this.createObject = function (data) {
-        var newObject = {};
+        var allObjects = {};
         for (let i = 0; i < data.length; i++) {
+            var newObject = {};
             for (let j = 0; j < data[i].attributes.length; j++) {
-                console.log("data[i].nodeName: " + data[i].attributes[j].nodeName + " | " + "data[i].nodeValue: " + data[i].attributes[j].nodeValue);
                 newObject[data[i].attributes[j].nodeName] = data[i].attributes[j].nodeValue;
             }
+            allObjects.push(newObject);
         }
-        return newObject;
+        console.log("In Factory(): " + allObjects[0].name);
+        return allObjects;
     };
 } 
